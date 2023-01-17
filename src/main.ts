0d4d7d7315ec642cc5e79ccfab0f03ddd8744ce4
mirkotorrisi <mirko.torrisi92@gmail.com>
2023-01-17 23:28:44 +0100

    add cors enabled

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('BOLLETTE API')
