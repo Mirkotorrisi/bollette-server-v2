69c4836d872f28ecdacc2f04ed471d0215679254
mirkotorrisi <mirko.torrisi92@gmail.com>
2023-01-20 22:18:05 +0100

    fixed missing try catch on validation token

--- src/interceptors/auth.interceptor.ts
+++ src/interceptors/auth.interceptor.ts
@@ -19,15 +19,18 @@ export class AuthInterceptor implements NestInterceptor {
   ): Promise<any> {
     const req = context.switchToHttp().getRequest();
     const token = req.headers['x-auth-token'];
+    console.log(token);
     if (!token) throw new UnauthorizedException('Auth token is missing');
-    const verified = jwt.verify(token, process.env.JWT_KEY);
-    const id = String(verified['id']);
-    const res = await this.redis.get(id);
-    if (res) {
-      this.redis.set(id, token, { EX: 600 });
-      req.user = verified;
-      return next.handle();
-    }
+    try {
+      const verified = jwt.verify(token, process.env.JWT_KEY);
+      const id = String(verified['id']);
+      const res = await this.redis.get(id);
+      if (res) {
+        this.redis.set(id, token, { EX: 600 });
+        req.user = verified;
+        return next.handle();
+      }
+    } catch (error) {}
     throw new UnauthorizedException(
       'Session expired. Log in again to continue',
     );
