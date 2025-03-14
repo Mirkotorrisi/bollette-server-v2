93cfee9e40b05b0d6b9b6f1cc71d404e066022db
mirkotorrisi <mirko.torrisi92@gmail.com>
2025-03-14 09:14:01 +0100

    refactor: streamline match fetching logic and error handling

--- src/championship/championship.service.ts
+++ src/championship/championship.service.ts
@@ -26,10 +26,6 @@ export class ChampionshipService {
     @Inject('REDIS') private redis: RedisClientType,
   ) {}
 
-  // onModuleInit() {
-  //   this.getBetflagMatches();
-  // }
-
   async getMatches(sport: ChampionshipEnum, avoidCache?: boolean) {
     this.logger.log('Get Matches');
     const cached = await this.redis.get(sport);
@@ -39,59 +35,61 @@ export class ChampionshipService {
       return JSON.parse(cached);
     }
 
-    // let availableBetList;
-    // if (sport === 'serie_a') {
-    //   availableBetList = await this.getBetflagMatches();
-    // } else {
-    const res = await firstValueFrom(
-      this.httpService
-        .get(`${THE_ODDS_API_URL + sport_keys[sport]}/odds/`, {
-          params: {
-            apiKey: process.env.THE_ODDS_API_KEY,
-            regions: 'eu',
-            markets: 'h2h,totals',
-          },
-        })
-        .pipe(
-          catchError((err) => {
-            this.logger.error('Something went wrong while fetching odds');
-            throw new BadRequestException(err.response.data);
-          }),
-        ),
-    );
+    let availableBetList;
+    if (sport === 'serie_a') {
+      availableBetList = await this.getBetflagMatches();
+    } else {
+      const res = await firstValueFrom(
+        this.httpService
+          .get(`${THE_ODDS_API_URL + sport_keys[sport]}/odds/`, {
+            params: {
+              apiKey: process.env.THE_ODDS_API_KEY,
+              regions: 'eu',
+              markets: 'h2h,totals',
+            },
+          })
+          .pipe(
+            catchError((err) => {
+              this.logger.error('Something went wrong while fetching odds');
+              throw new BadRequestException(err.response.data);
+            }),
+          ),
+      );
 
-    const availableBetList = res.data.map(
-      ({ home_team, away_team, bookmakers, commence_time, id }) => {
-        const bookmaker = bookmakers?.find((b) => b.markets.length > 1);
-        const h2h = bookmaker?.markets?.find((mkt) => mkt.key === 'h2h');
+      availableBetList = res.data.map(
+        ({ home_team, away_team, bookmakers, commence_time, id }) => {
+          const bookmaker = bookmakers?.find((b) => b.markets.length > 1);
+          const h2h = bookmaker?.markets?.find((mkt) => mkt.key === 'h2h');
 
-        const totals = bookmaker?.markets?.find((mkt) => mkt.key === 'totals');
+          const totals = bookmaker?.markets?.find(
+            (mkt) => mkt.key === 'totals',
+          );
 
-        const home = h2h?.outcomes.find((o) => o.name === home_team)?.price;
-        const away = h2h?.outcomes.find((o) => o.name === away_team)?.price;
-        const draw = h2h?.outcomes.find((o) => o.name === 'Draw')?.price;
-        const over = totals?.outcomes.find((o) => o.name === 'Over')?.price;
-        const under = totals?.outcomes.find((o) => o.name === 'Under')?.price;
-        const matchId = `${getTeamPrefix(home_team)}-${getTeamPrefix(
-          away_team,
-        )}${commence_time?.split('T')?.[0]}`;
-        return {
-          id,
-          sport_key: sport,
-          matchId,
-          teams: [home_team, away_team],
-          start: commence_time,
-          odds: {
-            home,
-            draw,
-            away,
-            over,
-            under,
-          },
-        };
-      },
-    );
-    // }
+          const home = h2h?.outcomes.find((o) => o.name === home_team)?.price;
+          const away = h2h?.outcomes.find((o) => o.name === away_team)?.price;
+          const draw = h2h?.outcomes.find((o) => o.name === 'Draw')?.price;
+          const over = totals?.outcomes.find((o) => o.name === 'Over')?.price;
+          const under = totals?.outcomes.find((o) => o.name === 'Under')?.price;
+          const matchId = `${getTeamPrefix(home_team)}-${getTeamPrefix(
+            away_team,
+          )}${commence_time?.split('T')?.[0]}`;
+          return {
+            id,
+            sport_key: sport,
+            matchId,
+            teams: [home_team, away_team],
+            start: commence_time,
+            odds: {
+              home,
+              draw,
+              away,
+              over,
+              under,
+            },
+          };
+        },
+      );
+    }
 
     this.redis.set(sport, JSON.stringify(availableBetList), {
       EX: 60,
@@ -115,9 +113,16 @@ export class ChampionshipService {
 
   async getBetflagMatches() {
     const data = await firstValueFrom(
-      this.httpService.get(BETFLAG_URL, {
-        headers: BETFLAG_HEADERS,
-      }),
+      this.httpService
+        .get(BETFLAG_URL, {
+          headers: BETFLAG_HEADERS,
+        })
+        .pipe(
+          catchError((err) => {
+            this.logger.error('Something went wrong while fetching odds');
+            throw new BadRequestException(err.response.data);
+          }),
+        ),
     );
     const parsed = parseBetflagMatches(data.data.leo);
     return parsed;
